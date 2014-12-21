abb1a0e7d1857cb5440f935e2aeb1b0329ce5f47
nihal2193 <nihal2193@gmail.com>
2014-12-21 16:09:38 +0530

    Comments added for clarity

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+## In this assignment we are trying to implement functions to cache the
+## operations (calculating inverse of a matrix) which are very costly and 
+## calculated repeatedly, so that next time if the matrix is same then the
+## is returned directly from the cache.
 
-## Write a short comment describing this function
+## The first function, `makeCacheMatrix` creates a special "matrix", which is
+##really a list containing a function to
+
+##1.  set the value of the matrix
+##2.  get the value of the matrix
+##3.  set the value of the inverse
+##4.  get the value of the inverse
+
 
 makeCacheMatrix <- function(x = matrix()) 
 {
@@ -18,7 +27,12 @@ makeCacheMatrix <- function(x = matrix())
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of the special "matrix"
+##created with the above function. However, it first checks to see if the
+##inverse has already been calculated. If so, it `get`s the inverse from the
+##cache and skips the computation. Otherwise, it calculates the inverse using
+##solve() function of the data and sets the value of the inverse in the cache
+##via the `setinv` function.
 
 cacheSolve <- function(x, ...)
 {
